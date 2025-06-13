a90ea2c84cfc21c363b2e601625d8e817fd9ab99
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-06-13 19:34:05 +0530

    made changes added fix for all the errors

--- app/api/product/seller-list/route.js
+++ app/api/product/seller-list/route.js
@@ -1,25 +1,22 @@
-import connectDb from "@/config/db"
-import authSeller from "@/lib/authSeller"
-import Product from "@/models/Product"
-import { auth } from "@clerk/nextjs"
-import { NextResponse } from "next/server"
+import connectDb from "@/config/db";
+import authSeller from "@/lib/authSeller";
+import Product from "@/models/Product";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export async function GET(req) {
-    try {
-        const { userId } = auth()
-        if (!userId) {
-            return NextResponse.json({ success: false, message: 'Unauthorized' })
-        }
-
+export async function GET(request) {
+    try{
+        const{userId} = getAuth(request)
         const isSeller = authSeller(userId)
-        if (!isSeller) {
-            return NextResponse.json({ success: false, message: 'Not authorized as seller' })
-        }
 
+        if(!isSeller){
+            return NextResponse.json({success : false, message : 'not authorized'})
+        }
         await connectDb()
         const products = await Product.find({})
-        return NextResponse.json({ success: true, products })
-    } catch (error) {
-        return NextResponse.json({ success: false, message: error.message })
+        return NextResponse.json({ success : true, products})
+
+    }catch(error){
+        return NextResponse.json({success : false, message : error.message})
     }
 }
