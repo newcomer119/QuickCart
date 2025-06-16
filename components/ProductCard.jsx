837244a91d9e7050feb1e9e27736550be9a7379a
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-06-16 19:37:22 +0530

    made changes added new things

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -20,9 +20,9 @@ const ProductCard = ({ product }) => {
     return (
         <div
             onClick={() => { router.push('/product/' + product._id); scrollTo(0, 0) }}
-            className="flex flex-col items-start gap-0.5 max-w-[200px] w-full cursor-pointer"
+            className="flex flex-col items-start gap-0.5 max-w-[280px] w-full cursor-pointer"
         >
-            <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-52 flex items-center justify-center">
+            <div className="cursor-pointer group relative bg-gray-500/10 rounded-lg w-full h-64 flex items-center justify-center">
                 <Image
                     src={product.image[0]}
                     alt={product.name}
