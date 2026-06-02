d9c73b7402b4374c586c21d3f1632bcfc597daab
kpuranjay <144197962+kpuranjay@users.noreply.github.com>
2026-06-02 13:20:46 +0530

    made changes

--- app/sitemap.jsx
+++ app/sitemap.jsx
@@ -13,9 +13,18 @@ export default function sitemap() {
   return [
     { url: base, lastModified: new Date() },
     ...shopUrls,
-    { url: `${base}/3d-printed-products`, lastModified: new Date() },
+    { url: `${base}/3d-printed-wordart`, lastModified: new Date() },
     { url: `${base}/3d-printing-filament`, lastModified: new Date() },
+    { url: `${base}/3d-printed-desk-essentials`, lastModified: new Date() },
+    { url: `${base}/3d-printed-keychain`, lastModified: new Date() },
+    { url: `${base}/3d-printed-lamps`, lastModified: new Date() },
+    { url: `${base}/3d-printed-decor-essentials`, lastModified: new Date() },
+    { url: `${base}/3d-printed-laptop-accessories`, lastModified: new Date() },
+    { url: `${base}/3d-printed-gaming-accessories`, lastModified: new Date() },
     { url: `${base}/contact`, lastModified: new Date() },
+    { url: `${base}/track-order`, lastModified: new Date() },
+    { url: `${base}/policies/cancellation`, lastModified: new Date() },
+    { url: `${base}/favicon.ico`, lastModified: new Date() },    
     { url: `${base}/faq`, lastModified: new Date() },
   ];
 }
