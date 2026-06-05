80945da7e97d1bff2e92c53d8de990ba837c99a7
kpuranjay <144197962+kpuranjay@users.noreply.github.com>
2026-06-05 19:13:08 +0530

    made changes

--- app/layout.js
+++ app/layout.js
@@ -47,6 +47,20 @@ export default function RootLayout({ children }) {
               })(window,document,'script','dataLayer','GTM-TNZ8PTKV');
             `}
           </Script>
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '3535356789973331');
+              fbq('track', 'PageView');
+            `}
+          </Script>
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
           <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
@@ -60,6 +74,15 @@ export default function RootLayout({ children }) {
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=3535356789973331&ev=PageView&noscript=1"
+              alt=""
             />
           </noscript>
           <Toaster />
