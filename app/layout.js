0356a251ec74a42af5d12c3b64204412d85848f2
kpuranjay <144197962+kpuranjay@users.noreply.github.com>
2026-06-05 14:14:55 +0530

    changes reverted

--- app/layout.js
+++ app/layout.js
@@ -37,15 +37,6 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <head>
-          <Script id="gtm-script" strategy="afterInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TNZ8PTKV');
-            `}
-          </Script>
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
           <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
